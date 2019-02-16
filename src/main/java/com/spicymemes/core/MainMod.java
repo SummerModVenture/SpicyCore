2295a9966e9f3799fe6a7511ae0760fd70582a6a
Zach <zachkozar@masterzach32.net>
2019-02-16 11:37:23 -0500

    Added kotlin adapter to coremod

--- src/main/java/com/spicymemes/core/MainMod.java
+++ src/main/java/com/spicymemes/core/MainMod.java
@@ -11,7 +11,7 @@ public class MainMod
 {
     public static final String MODID = "spicycore";
     public static final String NAME = "Spicy Core";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.2";
 
     private static Logger logger;
 
---
+++ src/main/kotlin/com/spicymemes/core/KotlinAdapter.kt
@@ -0,0 +1,28 @@
+package com.spicymemes.core
+
+import net.minecraftforge.fml.common.FMLModContainer
+import net.minecraftforge.fml.common.ILanguageAdapter
+import net.minecraftforge.fml.common.ModContainer
+import net.minecraftforge.fml.relauncher.Side
+import java.lang.reflect.Field
+import java.lang.reflect.Method
+
+class KotlinAdapter : ILanguageAdapter {
+
+    override fun setProxy(target: Field, proxyTarget: Class<*>, proxy: Any) = target.set(proxyTarget, proxy)
+
+    override fun getNewInstance(
+            container: FMLModContainer,
+            objectClass: Class<*>,
+            classLoader: ClassLoader,
+            factoryMarkedAnnotation: Method?
+    ) = when {
+        factoryMarkedAnnotation != null -> factoryMarkedAnnotation(null)
+        objectClass.fields.any { it.name == "INSTANCE" } -> objectClass.getField("INSTANCE").get(null)
+        else -> objectClass.newInstance()
+    }!!
+
+    override fun supportsStatics() = false
+
+    override fun setInternalProxies(mod: ModContainer?, side: Side?, loader: ClassLoader?) = Unit
+}
