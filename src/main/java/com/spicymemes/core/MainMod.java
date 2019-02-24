074b6a52b6ca39ffa9778b40dd8d6f65c2329163
Zach <zachkozar@masterzach32.net>
2019-02-23 19:03:46 -0500

    Added generic packet handler

--- src/main/java/com/spicymemes/core/MainMod.java
+++ src/main/java/com/spicymemes/core/MainMod.java
@@ -11,9 +11,9 @@ public class MainMod
 {
     public static final String MODID = "spicycore";
     public static final String NAME = "Spicy Core";
-    public static final String VERSION = "1.0.5";
+    public static final String VERSION = "1.0.6";
 
-    private static Logger logger;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
