8fb7e40c1599233d20bd4442b04b1d962ee280c1
Zachary Kozar <zachk@vt.edu>
2020-09-30 22:11:31 -0400

    Bug fixes

--- src/main/kotlin/com/spicymemes/core/extensions/PlayerEntity.kt
+++ src/main/kotlin/com/spicymemes/core/extensions/PlayerEntity.kt
@@ -1,28 +1,8 @@
 package com.spicymemes.core.extensions
 
-import com.spicymemes.core.MainMod.*
 import net.minecraft.client.entity.player.*
 import net.minecraft.entity.player.*
-import java.lang.IllegalStateException
 
-fun PlayerEntity.toServerPlayerEntity(): ServerPlayerEntity {
-    return try {
-        this as ServerPlayerEntity
-    } catch (e: Throwable) {
-        "Could not cast PlayerEntity to ServerPlayerEntity.".let {
-            logger.error(it, e)
-            throw IllegalStateException(it, e)
-        }
-    }
-}
+fun PlayerEntity.toServerPlayerEntity(): ServerPlayerEntity = this as ServerPlayerEntity
 
-fun PlayerEntity.toClientPlayerEntity(): ClientPlayerEntity {
-    return try {
-        this as ClientPlayerEntity
-    } catch (e: Throwable) {
-        "Could not cast PlayerEntity to ClientPlayerEntity.".let {
-            logger.error(it, e)
-            throw IllegalStateException(it, e)
-        }
-    }
-}
+fun PlayerEntity.toClientPlayerEntity(): ClientPlayerEntity = this as ClientPlayerEntity
