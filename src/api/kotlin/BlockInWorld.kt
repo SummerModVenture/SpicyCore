949ea9960a9d00973d6aa16619d38bb178cfd283
Zach Kozar <zachkozar@vt.edu>
2021-08-03 23:08:22 -0400

    Rename tile to blockEntity

--- src/api/kotlin/BlockInWorld.kt
+++ src/api/kotlin/BlockInWorld.kt
@@ -7,11 +7,18 @@ import net.minecraft.world.level.block.entity.*
 import net.minecraft.world.level.block.state.*
 
 /**
- * Helper class for packaging world/block/state in one object
+ * Represents the state of a block in a level at the time it was grabbed.
  */
 data class BlockInWorld(val world: Level, val pos: BlockPos) {
 
     val state: BlockState by lazy { world.getBlockState(pos) }
     val block: Block = state.block
-    val tile: BlockEntity? by lazy { world.getBlockEntity(pos) }
+    val blockEntity: BlockEntity? by lazy { world.getBlockEntity(pos) }
+    @Deprecated("Use blockEntity property.")
+    val tile: BlockEntity? by this::blockEntity
 }
+
+/**
+ * Gets the [BlockInWorld] at the specified [pos]
+ */
+fun Level.getBlock(pos: BlockPos) = BlockInWorld(this, pos)
--- src/api/kotlin/extensions/World.kt
+++
@@ -1,7 +0,0 @@
-package com.spicymemes.api.extensions
-
-import com.spicymemes.api.*
-import net.minecraft.core.*
-import net.minecraft.world.level.*
-
-fun Level.getBlock(pos: BlockPos) = BlockInWorld(this, pos)
