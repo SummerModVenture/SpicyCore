473894315005ffb7dcb4560273ac5268398a3dda
Zach Kozar <zachkozar@vt.edu>
2021-08-04 19:11:44 -0400

    Bug fixes

--- src/api/kotlin/config/ConfigBuilder.kt
+++ src/api/kotlin/config/ConfigBuilder.kt
@@ -3,14 +3,19 @@ package com.spicymemes.api.config
 import net.minecraftforge.common.*
 import kotlin.contracts.*
 
-fun config(name: String, config: ForgeConfigSpec.Builder.() -> Unit): ForgeConfigSpec {
+@DslMarker
+annotation class ConfigDsl
+
+@ConfigDsl
+inline fun config(name: String, config: ForgeConfigSpec.Builder.() -> Unit): ForgeConfigSpec {
     contract { callsInPlace(config, InvocationKind.EXACTLY_ONCE) }
     return ForgeConfigSpec.Builder()
         .apply { section(name, config) }
         .build()
 }
 
-fun ForgeConfigSpec.Builder.section(name: String, config: ForgeConfigSpec.Builder.() -> Unit) {
+@ConfigDsl
+inline fun ForgeConfigSpec.Builder.section(name: String, config: ForgeConfigSpec.Builder.() -> Unit) {
     contract { callsInPlace(config, InvocationKind.EXACTLY_ONCE) }
     push(name)
     config()
